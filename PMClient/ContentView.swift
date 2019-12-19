636375fd326b8e0b154ec3b7f4cf533fc0f299de
Frederick Kuhl <fkuhl@tyndalesoft.com>
2019-12-19 15:00:45 -0500

    Initial Commit

---
+++ PMClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PMClient
+//
+//  Created by Frederick Kuhl on 12/19/19.
+//  Copyright © 2019 TyndaleSoft LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
